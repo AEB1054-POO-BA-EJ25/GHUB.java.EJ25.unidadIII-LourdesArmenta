40ee482214e9cc00b44cc2895a87aa4fbfb31b73
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-04-03 15:04:52 +0000

    conversiones de tipo primitivo

---
+++ conversiones/TestConversiones.java
@@ -0,0 +1,14 @@
+package conversiones;
+
+public class TestConversiones {
+    public static void main(String[] args) {
+        int i = 5;
+        double y = (double)i;
+        System.out.println("valor de variable y:"+y); //coversion de tipos
+        double x = 5.1516;
+        int z = (int)x;
+        System.out.println("valor de variable z:"+z); //coversion de tipos explicita
+
+    }
+    
+}
---
+++ fecha/TestFechaDetallada.java
@@ -0,0 +1,29 @@
+package fecha;
+
+import java.util.Scanner;
+
+public class TestFechaDetallada {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese una Fecha (dd/mm/aaaa):");
+        String sFecha = entrada.nextLine();
+
+        //creamos el objeto
+        FechaDetallada f = new FechaDetallada(sFecha);
+
+        //mostra por pantala
+        System.out.println("La Fecha ingresada es:"+f);
+
+        //el usuario ingresara los dias a suma
+        System.out.println("Ingrese una cantidad de dias para sumar (puede ser negativo):");
+        int diasSum = entrada.nextInt();
+
+        //le sumanos esos dias a la fecha
+        f.addDias(diasSum);
+
+        //mostramos la nueva fecha (con los dias sumados)
+        System.out.println("sumando "+diasSum+" dias, queda: "+f);
+        
+    }
+    
+}
